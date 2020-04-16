10bb385228ad3c21f695465117cda2922324d711
Teddy Kahwaji <teddykahwaji86@gmail.com>
2020-04-16 02:17:17 -0500

    Updates

---
+++ precache-manifest.e3c534992beb2fa15f1095f6331cfc39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "492a719239679c9ce529a99603797bec",
+    "url": "/Covid19_Statistics/index.html"
+  },
+  {
+    "revision": "4252cf74f68309797362",
+    "url": "/Covid19_Statistics/static/css/main.cee2310a.chunk.css"
+  },
+  {
+    "revision": "c052dcb531b03a5f6c83",
+    "url": "/Covid19_Statistics/static/js/2.1ba349d5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19_Statistics/static/js/2.1ba349d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4252cf74f68309797362",
+    "url": "/Covid19_Statistics/static/js/main.6089afb4.chunk.js"
+  },
+  {
+    "revision": "da961409fa27d53e25e5",
+    "url": "/Covid19_Statistics/static/js/runtime-main.a155558f.js"
+  }
+]);
